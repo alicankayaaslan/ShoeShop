bcba18b9f14dba4db299d5679d67ac73ae77fd35
Ali Can Kayaaslan <ackayaaslan@gmail.com>
2023-03-03 14:47:36 +0300

    Initial Commit

---
+++ ShoeShop/ShoeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeShopApp.swift
+//  ShoeShop
+//
+//  Created by Ali Can Kayaaslan on 3.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
